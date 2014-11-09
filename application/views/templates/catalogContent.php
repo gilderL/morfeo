570a2a27c0dd92b1222edca661703722a694a86b
FelixAdrian <felix.castillo@uabc.edu.mx>
2014-11-09 15:14:17 -0800

    vistas
    agrego vistas de el catalogo de juegos
    en la vista de head.php añadi la imagen de fondo pero me sale como
    archivo partido ala mitad nose ve, tambien para el carrusel tengo
    problemas para lque aparescan las imagenes

---
+++ application/views/templates/catalogContent.php
@@ -0,0 +1,51 @@
+<div id="Content">
+   <div id="cataMenu"></div>
+		
+        
+		  <div id ="Game1" class="catalogGames"> <div id="Img1"class="catalogImg"></div>
+				<div id="cataText1" class="cataText"></div></div>
+          
+		  <div id="Game2"class="catalogGames">
+		   
+		    <div id="Img2"class="catalogImg"></div>
+			  <div id="cataText2"class="cataText"></div>
+	      
+		  </div>
+            
+		  <div id="Game3"class="catalogGames">
+		    <div id="Img3"class="catalogImg"></div>
+			  <div id="cataText3"class="cataText"></div>
+	      </div>
+		
+          <div id="Game4"class="catalogGames">
+		    <div id="Img4"class="catalogImg"></div>
+			  <div id="CataText4"class="cataText"></div>
+	      </div>
+          
+		  <div id="Game5"class="catalogGames">
+			<div id="Img5"class="catalogImg"></div>
+				<div id="cataText5"class="cataText"></div>
+		  </div>
+          
+		  <div id="Game6"class="catalogGames">
+			<div id="Img6"class="catalogImg"></div>
+			<div id="cataText6"class="cataText"></div>
+     </div>
+            
+    <div id="Game7" class="catalogGames">
+          <div id="Img7"class="catalogImg"></div>
+		<div id="cataText7"class="cataText"></div>
+        </div>
+        
+    <div class="catalogGames" id="Game8">
+           <div id="Img8"class="catalogImg"></div>
+		<div id="cataText8"class="cataText"></div>
+          </div>
+            
+            
+		  
+  </div>
+
+		
+	
+ 
---
+++ application/views/templates/loggedHeader.php
@@ -0,0 +1,9 @@
+<div id="Header">  
+<div id="Logo" class="logo"></div>
+    <div id="Search"></div>
+    <div id="myAccount"></div>
+    <div id="welcome">
+      <div id="userName"></div>
+    </div>
+    </div>
+    
