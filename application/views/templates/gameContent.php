a46c4fb669f288f396d3d37f7e3050f109df72d2
FelixAdrian <felix.castillo@uabc.edu.mx>
2014-11-17 13:55:14 -0800

    vistas de juego
    subi la vistas de el juego cuando se esta apunto de comprar  aun falta
    el ccs esta en progreso

---
+++ application/views/templates/gameContent.php
@@ -0,0 +1,15 @@
+ <div id="Content">
+   <div id="gameInfo">
+     <div id="price">
+       <div id="buyButton"></div>
+     </div>
+     <div id="gameImg"></div>
+     <div id="description"></div>
+   </div>
+ 
+
+  <div id="reqCont">
+    <div id="resum"></div>
+    <div id="req"></div>
+  </div>
+  </div>
