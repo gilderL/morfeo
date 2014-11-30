84bd1290953e779e96391c072a055892167740d8
Gilder librado <gilder.librado@gmail.com>
2014-11-30 12:55:31 -0800

    Create library.php

---
+++ application/views/templates/library.php
@@ -0,0 +1,92 @@
+<?php
+/********************
+*File name: library.php
+*System Name: morfeo
+*Module Name: 
+*Class Name:
+*Dependencies: View			
+*Revision: 2014-11-22
+*Author: gilder librado
+*e-mail Author: gilder.librado@uabc.edu.mx
+*Copyright Notice: Derechos reservados, UABC 2014
+*********************/
+?>
+ <div id="Content">
+	<div id='tit' class="text">MY GAMES</div>
+<div id="list" class="terms">
+	<div id="gam1" class="gam gamCol2"> 
+		<div id="gam2" class="styleFue">Gems of War</div>
+		<div id="gam3" class="styleFue"> download</div>
+		<div id="gam4" class="styleFue" ><a href="https://www.youtube.com/watch?v=vd4jedjgMag&channel=UCaih2OjUBoGYmMM-4ooPvBA">view trailer</a> </div>
+	</div>
+		
+	<div id="gam1" class="gam gamCol1">
+		<div id="gam2" class="styleFue">Far Cry 4</div>
+		<div id="gam3" class="styleFue"> download</div>
+		<div id="gam4" class="styleFue" ><a href="https://www.youtube.com/watch?v=6d60v1OErEY&channel=IGNentertainment">view trailer</a> </div>
+	</div>
+		
+	<div id="gam1" class="gam gamCol2"> 
+		<div id="gam2" class="styleFue">Call of Duty: Advanced Warfare</div>
+		<div id="gam3" class="styleFue"> download</div>
+		<div id="gam4" class="styleFue" ><a href="https://www.youtube.com/watch?v=GccGbdLqTmQ">view trailer</a> </div>
+	</div>
+		
+	<div id="gam1" class="gam gamCol1">
+		<div id="gam2" class="styleFue">Overwatch</div>
+		<div id="gam3" class="styleFue">download</div>
+		<div id="gam4" class="styleFue" ><a href="https://www.youtube.com/watch?v=FqnKB22pOC0&channel=UClOf1XXinvZsy4wKPAkro2A">view trailer</a> </div>
+	</div>
+		
+	<div id="gam1" class="gam gamCol2"> 
+		<div id="gam2" class="styleFue">Battlefield Hardline</div>
+		<div id="gam3" class="styleFue">download</div>
+	</div>
+		
+	<div id="gam1" class="gam gamCol1"> 
+		<div id="gam2" class="styleFue">Battlefield 4</div>
+		<div id="gam3" class="styleFue"> download</div>
+	</div>
+		
+	<div id="gam1" class="gam gamCol2"> 
+		<div id="gam2" class="styleFue">Left 4 Dead</div>
+		<div id="gam3" class="styleFue">download</div>
+	</div>
+		
+	<div id="gam1" class="gam gamCol1"> 
+		<div id="gam2" class="styleFue">DayZ</div>
+		<div id="gam3" class="styleFue">download</div>
+	</div>
+		
+	<div id="gam1" class="gam gamCol2">
+		<div id="gam2" class="styleFue">Titanfall</div>
+		<div id="gam3" class="styleFue"> download</div>
+	</div>
+		
+	<div id="gam1" class="gam gamCol1"> 
+		<div id="gam2" class="styleFue">Dead Island 2</div>
+		<div id="gam3" class="styleFue"> download</div>
+	</div>
+		
+	<div id="gam1" class="gam gamCol2"> 
+		<div id="gam2" class="styleFue">ArmA 3</div>
+		<div id="gam3" class="styleFue">download</div>
+	</div>
+		
+	<div id="gam1" class="gam gamCol1">
+		<div id="gam2" class="styleFue">Battlefield 3</div>
+		<div id="gam3" class="styleFue"> download</div>
+	</div>
+	
+	<div id="gam1" class="gam gamCol2"> 
+		<div id="gam2" class="styleFue">Call of Duty: Black Ops 2</div>
+		<div id="gam3" class="styleFue"> download</div>
+	</div>
+	
+	<div id="gam1" class="gam gamCol1"> 
+		<div id="gam2" class="styleFue">Sniper Elite 3</div>
+		<div id="gam3" class="styleFue"> download</div>
+	</div>
+		
+	</div>
+</div>
